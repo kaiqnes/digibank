3775071828d3dc3daa5d6161f876e12a11589759
kaiqnes <caique.nunes.ext@jumia.com>
2022-05-05 18:16:13 -0300

    add function to create db from code

--- internal/frameworks/database/database.go
+++ internal/frameworks/database/database.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"database/sql"
 	"digibank/internal/domain/entities"
 	"digibank/internal/frameworks/utils"
 	"fmt"
@@ -10,7 +11,9 @@ import (
 )
 
 func NewSession() *gorm.DB {
-	dns := getDNS()
+	createDatabase()
+
+	dns := getFullDNS()
 	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect database - err %v", err)
@@ -19,13 +22,27 @@ func NewSession() *gorm.DB {
 	return db
 }
 
+func createDatabase() {
+	db, err := sql.Open("mysql", getShortDNS())
+	if err != nil {
+		log.Fatalf("failed to connect database - err %v", err)
+	}
+	defer db.Close()
+
+	name := utils.GetEnvOrDefault("DB_NAME", "")
+	_, err = db.Exec(fmt.Sprintf("CREATE DATABASE %s", name))
+	if err != nil {
+		log.Fatalf("failed to create %s database - err %v", name, err)
+	}
+}
+
 func migrateModels(db *gorm.DB) {
 	db.AutoMigrate(&entities.Account{})
 	db.AutoMigrate(&entities.OperationType{})
 	db.AutoMigrate(&entities.Transaction{})
 }
 
-func getDNS() string {
+func getFullDNS() string {
 	user := utils.GetEnvOrDefault("DB_USER", "")
 	pass := utils.GetEnvOrDefault("DB_PASS", "")
 	host := utils.GetEnvOrDefault("DB_HOST", "")
@@ -34,4 +51,14 @@ func getDNS() string {
 
 	dnsTemplate := "%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local"
 	return fmt.Sprintf(dnsTemplate, user, pass, host, port, name)
+}
+
+func getShortDNS() string {
+	user := utils.GetEnvOrDefault("DB_USER", "")
+	pass := utils.GetEnvOrDefault("DB_PASS", "")
+	host := utils.GetEnvOrDefault("DB_HOST", "")
+	port := utils.GetEnvOrDefault("DB_PORT", "")
+
+	dnsTemplate := "%s:%s@tcp(%s:%s)/"
+	return fmt.Sprintf(dnsTemplate, user, pass, host, port)
 }
