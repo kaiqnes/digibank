41eca420b19a1d5b324ec481602f1d8c759d643a
kaiqnes <caique.nunes.ext@jumia.com>
2022-05-05 14:59:38 -0300

    implement healthCheck unit test

--- internal/interfaceAdapters/controllers/healthCheckController_test.go
+++ internal/interfaceAdapters/controllers/healthCheckController_test.go
@@ -1,1 +1,34 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/assert/v2"
+	"github.com/golang/mock/gomock"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHealthCheckController_HealthCheck(t *testing.T) {
+	scenarios := []healthCheckControllerScenario{
+		makeScenarioThatGetHealthCheck(),
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.TestName, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			router := gin.Default()
+
+			testController := NewHealthCheckController(router)
+			testController.SetupEndpoints()
+
+			response := httptest.NewRecorder()
+			executeRequest(response, scenario.Method, getFullUrl(scenario.Uri, ""), emptyBody, router)
+
+			ctrl.Finish()
+
+			assert.Equal(t, response.Body.String(), scenario.ExpectResponse)
+			assert.Equal(t, response.Result().StatusCode, scenario.ExpectStatus)
+		})
+	}
+}
